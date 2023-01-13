024f2fba25db7f0a5736892eb81a5701990f71d5
Ayhem <ja_allouache@esi.dz>
2023-01-13 17:24:58 +0100

    fix upload file

--- config/db_connection.php
+++ config/db_connection.php
@@ -1,11 +1,10 @@
 <?php
-    $host = 'mysql:host = localhost ; dbname = iot';
-    $login = 'root';
-    $password = '';
+$host = 'mysql:host = localhost ; dbname = iot';
+$login = 'allprotas';
+$password = '131069aa';
 try {
     $connection = new PDO($host, $login, $password);
     // $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-}
-catch(PDOException $e){
-    echo "error in connecting".$e->getMessage();
+} catch (PDOException $e) {
+    echo "error in connecting" . $e->getMessage();
 }
--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <footer>
     <p class="text-center">Copyright &copy; Allouache-Satoutah</p>
 </footer>
-<script src="JS/search.js"></script>
 <script src="JS/modal.js"></script>
 </body>
 
